34215bd0ac9d1800af174f6a47df2c017bbc917f
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-10-14 20:04:41 +0700

    SETUP LOCAL DI VPS FELIX

--- src/config/database.js
+++ src/config/database.js
@@ -5,23 +5,32 @@ const env = (process.env.PG_ENV || "windows").toLowerCase();
 const connections = {
   windows: {
     host: "localhost",
-
     // Ini setup laptop sendi
     // port: 5433,
+    // user: "postgres",
+    // password: "super.admin",
+    // database: "rimba_main",
 
     // Ini setup laptop reza
     port: 5432,
-
     user: "postgres",
     password: "super.admin",
     database: "rimba_main",
   },
   linux: {
+    // Ini setup laptop sendi
+    // host: "localhost",
+    // port: 5432,
+    // user: "user_rimba",
+    // password: "password_kuat",
+    // database: "main_rimba",
+
+    // Ini setup laptop reza
     host: "localhost",
     port: 5432,
-    user: "user_rimba",
-    password: "password_kuat",
-    database: "main_rimba",
+    user: "postgres",
+    password: "super.admin",
+    database: "rimba_main",
   },
 };
 
