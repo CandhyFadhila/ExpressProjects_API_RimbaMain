7db356e9d584a7153672d957a044e51d1c8ff588
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-11-21 19:49:35 +0700

    FIXED: Refactor and improve validation on get all topics if type is “Training”

--- src/routes/kmis/learningAttemptRoutes.js
+++ src/routes/kmis/learningAttemptRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const logger = require("../../utils/logger");
 const studentLearningCourseController = require("../../controllers/kmis/studentLearningCourseController");
 const {
   storeLearningAttemptValidator,
