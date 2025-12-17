161360ed64734dcc36f1db8cd4731bb23261e1a2
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-12-18 05:24:00 +0700

    FIXED: Modify cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["https://rimba-atrbpn-admin.vercel.app", "https://rimba-atrbpn.vercel.app", "https://rimba.webgis.app", "https://rimba.webgis.app"],
+  origin: ["https://rimba.webgis.app", "https://rimba.webgis.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
