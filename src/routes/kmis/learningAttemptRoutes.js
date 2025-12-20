88519e6ce1a65b39005cfcdae44b13208bacfc78
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-12-20 13:05:42 +0700

    FIXED: Add authentication check before getting topic_type “Pelatihan”

--- src/routes/kmis/learningAttemptRoutes.js
+++ src/routes/kmis/learningAttemptRoutes.js
@@ -9,6 +9,7 @@ const {
 } = require("../../validators/kmis/updateFeedbackValidator");
 const validate = require("../../middlewares/validate");
 const authMiddleware = require("../../middlewares/authMiddleware");
+const { authOptionalMiddleware } = require("../../middlewares/authMiddleware");
 const rateLimiter = require("../../middlewares/rateLimitMiddleware");
 const requirePermission = require("../../middlewares/requirePermission");
 const requireAbility = require("../../middlewares/requireAbility");
@@ -19,6 +20,7 @@ const upload = multer();
 router.get(
   "/get-material-public/:id",
   rateLimiter,
+  authOptionalMiddleware,
   studentLearningCourseController.getPublicMaterialbyId
 );
 
